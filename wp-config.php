d108eb829b5f6a366d4c085841cf573b044b1551
Joe Hoyle <joehoyle@gmail.com>
2019-01-31 12:22:21 +0800

    Add wp-config.php specific to local-server

---
+++ wp-config.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( empty( $_SERVER['HTTP_HOST'] ) ) {
+	$_SERVER['HTTP_HOST'] = get_env( 'COMPOSE_PROJECT_NAME' ) . '.hmdocker';
+}
+
+define( 'DB_HOST', getenv( 'DB_HOST' ) );
+define( 'DB_USER', getenv( 'DB_USER' ) );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
+define( 'DB_NAME', getenv( 'DB_NAME' ) );
+
+global $redis_server;
+$redis_server = [
+	'host' => getenv( 'REDIS_HOST' ),
+	'port' => getenv( 'REDIS_PORT' ),
+];
+
+$table_prefix = getenv( 'TABLE_PREFIX' ) ?: 'wp_';
