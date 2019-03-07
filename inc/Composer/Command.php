7a73f4c93d6f7cb9a2f05f6661e11108c51692ac
Joe Hoyle <joehoyle@gmail.com>
2019-03-07 14:00:37 +0530

    Use process

--- inc/Composer/Command.php
+++ inc/Composer/Command.php
@@ -5,6 +5,7 @@ namespace HM\Platform\LocalServer\Composer;
 use Composer\Command\BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class Command extends BaseCommand {
 	protected function configure() {
@@ -16,6 +17,21 @@ class Command extends BaseCommand {
 		if ( ! file_exists( '/etc/resolver/hmdocker' ) ) {
 			`sh vendor/humanmade/local-server/docker/bin/install.sh`;
 		}
-		`NAME=$(basename "\$PWD") ; cd vendor/humanmade/local-server/docker && docker-compose -f proxy.yml up -d && VOLUME=../../../../ COMPOSE_PROJECT_NAME=\$NAME docker-compose up && echo "Started." && open "http://\$NAME.hmdocker"`;
+
+		$proxy = new Process( 'docker-compose -f proxy.yml up', 'vendor/humanmade/local-server/docker' );
+		$proxy->start();
+
+		$compose = new Process( 'docker-compose up', 'vendor/humanmade/local-server/docker', [
+			'VOLUME' => getcwd(),
+			'COMPOSE_PROJECT_NAME' => basename( getcwd() ),
+		] );
+
+		$compose->start( function ( $type, $buffer ) {
+			echo $buffer;
+		} );
+
+		$compose->wait();
+
+		//`NAME=$(basename "\$PWD") ; cd vendor/humanmade/local-server/docker && docker-compose -f proxy.yml up -d && VOLUME=../../../../ COMPOSE_PROJECT_NAME=\$NAME docker-compose up && echo "Started." && open "http://\$NAME.hmdocker"`;
 	}
 }
