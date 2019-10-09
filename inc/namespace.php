3fbf99b47af3955a45faaca7aefe1a40776224a8
tomjn <contact@tomjn.com>
2019-10-09 19:09:33 +0100

    tell qm how to adjust paths in stack traces to use the host machines file paths

--- inc/namespace.php
+++ inc/namespace.php
@@ -75,4 +75,29 @@ function bootstrap() {
 		define( 'ALTIS_ANALYTICS_PINPOINT_ENDPOINT', getenv( 'ALTIS_ANALYTICS_PINPOINT_ENDPOINT' ) );
 		define( 'ALTIS_ANALYTICS_COGNITO_ENDPOINT', getenv( 'ALTIS_ANALYTICS_COGNITO_ENDPOINT' ) );
 	}
+
+	add_filter( 'qm/output/file_path_map', __NAMESPACE__ . '\\set_file_path_map', 1 );
+}
+
+/**
+ * Enables Query Monitor to map paths to their
+ * original values on the host.
+ *
+ * @param array $map an associative array of folders as keys, and their mappings as values
+ * @return array the adjusted mapping of folders
+ */
+function set_file_path_map( $map ) : array {
+	if ( ! file_exists( '/etc/chassis-constants' ) ) {
+		return $map;
+	}
+	$json_string = file_get_contents( '/etc/chassis-constants' );
+	$data = json_decode( $json_string, true );
+	if ( empty( $data['synced_folders'] ) ) {
+		return $map;
+	}
+	foreach ( $data['synced_folders'] as $guest => $host ) {
+		$map[ $guest ] = $host;
+	}
+
+	return $map;
 }
