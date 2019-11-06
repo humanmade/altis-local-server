16c4ab11d7ebd7f80405495da2a21796661b007e
Robert O'Rourke <robert@o-rourke.org>
2019-11-06 11:18:54 +0000

    merge master properly

--- inc/namespace.php
+++ inc/namespace.php
@@ -93,20 +93,6 @@ function bootstrap() {
  * @return array Adjusted mapping of folders
  */
 function set_file_path_map( array $map ) : array {
-	if ( ! file_exists( '/etc/chassis-constants' ) ) {
-		return $map;
-	}
-	$json_string = file_get_contents( '/etc/chassis-constants' );
-	$data = json_decode( $json_string, true );
-	if ( empty( $data ) ) {
-		return $map;
-	}
-	if ( empty( $data['synced_folders'] ) ) {
-		return $map;
-	}
-	foreach ( $data['synced_folders'] as $guest => $host ) {
-		$map[ $guest ] = $host;
-	}
-
+	$map['/usr/src/app/'] = '';
 	return $map;
 }
