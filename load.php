1584c7b3159de9977e794c2a8cca5af1b964f975
Robert O'Rourke <robert@o-rourke.org>
2020-02-17 12:34:16 +0000

    split out register callback before function guard

--- load.php
+++ load.php
@@ -7,12 +7,7 @@ use function Altis\register_module;
 
 require_once __DIR__ . '/inc/namespace.php';
 
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
-add_action( 'altis.modules.init', function () {
+function register() {
 	$default_settings = [
 		'enabled' => get_environment_architecture() === 'local-server',
 		's3' => true,
@@ -21,4 +16,11 @@ add_action( 'altis.modules.init', function () {
 	];
 
 	register_module( 'local-server', __DIR__, 'Local Server', $default_settings, __NAMESPACE__ . '\\bootstrap' );
-} );
+}
+
+// Don't self-initialize if this is not an Altis execution.
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
+add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
