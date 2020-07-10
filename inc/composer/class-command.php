c8b14e722770a17eee9d62cb546d7b3f535d9b4e
Paul de Wouters <paul@humanmade.com>
2020-07-10 09:42:32 +0100

    CR fix

--- inc/composer/class-command.php
+++ inc/composer/class-command.php
@@ -14,8 +14,8 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Process\Process;
 
 /**
  * Altis Local Server Composer Command.
