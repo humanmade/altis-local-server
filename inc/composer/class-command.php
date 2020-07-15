c986e23a1999cc82c87fb6022f9c19291f56fd6b
Robert O'Rourke <robert@o-rourke.org>
2020-07-15 14:23:02 +0100

    Check OS family to configure XDebug remote host

--- inc/composer/class-command.php
+++ inc/composer/class-command.php
@@ -107,6 +107,9 @@ EOT
 
 		if ( $input->getOption( 'xdebug' ) ) {
 			$env['PHP_IMAGE'] = 'humanmade/altis-local-server-php:3.1.0-dev';
+			if ( in_array( PHP_OS_FAMILY, [ 'BSD', 'Linux', 'Solaris', 'Unknown' ], true ) ) {
+				$env['XEBUG_REMOTE_HOST'] = '172.17.0.1';
+			}
 		}
 
 		$compose = new Process( 'docker-compose up -d', 'vendor/altis/local-server/docker', $env );
