0c1a8f49c542aa5577ffa794e8cc9ce650856a2b
Fernando Schubert <fernando@humanmade.com>
2023-08-11 12:08:09 -0300

    Release 3.5.4 altis-local-server-nginx

--- inc/composer/class-docker-compose-generator.php
+++ inc/composer/class-docker-compose-generator.php
@@ -298,7 +298,7 @@ class Docker_Compose_Generator {
 
 		return [
 			'nginx' => [
-				'image' => 'humanmade/altis-local-server-nginx:3.5.2',
+				'image' => 'humanmade/altis-local-server-nginx:3.5.4',
 				'container_name' => "{$this->project_name}-nginx",
 				'networks' => [
 					'proxy',
