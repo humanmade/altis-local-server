1258923420f33789075740f381234f59a0718993
Mike Little <mike@zed1.com>
2025-10-30 17:56:16 +0000

    [Backport] Update PHP versions to latest Docker tags
    Backport https://github.com/humanmade/altis-local-server/pull/855 to v21-branch

--- inc/composer/class-docker-compose-generator.php
+++ inc/composer/class-docker-compose-generator.php
@@ -98,9 +98,9 @@ class Docker_Compose_Generator {
 	 */
 	protected function get_php_reusable() : array {
 		$version_map = [
-			'8.3' => 'humanmade/altis-local-server-php:8.3.11',
-			'8.2' => 'humanmade/altis-local-server-php:8.2.25',
-			'8.1' => 'humanmade/altis-local-server-php:6.0.22',
+			'8.3' => 'humanmade/altis-local-server-php:8.3.17',
+			'8.2' => 'humanmade/altis-local-server-php:8.2.31',
+			'8.1' => 'humanmade/altis-local-server-php:6.0.27',
 		];
 
 		$versions = array_keys( $version_map );
