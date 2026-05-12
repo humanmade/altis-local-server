f567ab1cb8bde26a06f410b1fefd0e29ddea1839
Mike Little <mike@zed1.com>
2026-05-12 17:18:47 +0100

    Update PHP images to latest patch releases
    (cherry picked from commit df2bd3e16cc5cfcf234223ffb0ed1151221eb838)

--- inc/composer/class-docker-compose-generator.php
+++ inc/composer/class-docker-compose-generator.php
@@ -107,9 +107,9 @@ class Docker_Compose_Generator {
 	 */
 	protected function get_php_reusable() : array {
 		$version_map = [
-			'8.4' => 'humanmade/altis-local-server-php:8.4.4',
-			'8.3' => 'humanmade/altis-local-server-php:8.3.23',
-			'8.2' => 'humanmade/altis-local-server-php:8.2.36',
+			'8.4' => 'humanmade/altis-local-server-php:8.4.9',
+			'8.3' => 'humanmade/altis-local-server-php:8.3.24',
+			'8.2' => 'humanmade/altis-local-server-php:8.2.37',
 			'8.1' => 'humanmade/altis-local-server-php:6.0.30',
 		];
 
