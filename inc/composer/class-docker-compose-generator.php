0b964ce6ac6d4c747118f16be2ebad323ea024a7
Mike Little <mike@zed1.com>
2026-07-21 09:20:26 +0100

    Update nginx container
    Updates the nginx container to version 3.7.4
    
    (cherry picked from commit d1a9ed3b8aa090492de6f1c4b350fe8ccf4b171a)

--- inc/composer/class-docker-compose-generator.php
+++ inc/composer/class-docker-compose-generator.php
@@ -350,7 +350,7 @@ class Docker_Compose_Generator {
 
 		return [
 			'nginx' => [
-				'image' => 'humanmade/altis-local-server-nginx:3.7.3',
+				'image' => 'humanmade/altis-local-server-nginx:3.7.4',
 				'container_name' => "{$this->project_name}-nginx",
 				'networks' => [
 					'proxy',
